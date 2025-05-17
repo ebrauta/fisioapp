404e8ddf9fab5ee4dc1c5e5c0879964fe5502ea0
ebrauta <ebrauta@gmail.com>
2025-05-17 16:47:16 -0300

    feat: acertos em PatientList e PatientListItem com ordenação da lista e inserção dos componentes  UI

--- components/PatientList/index.tsx
+++ components/PatientList/index.tsx
@@ -3,7 +3,7 @@ import { Colors } from "@/constants/Colors"
 import { LIST_ITEM_HEIGHT } from "@/constants/Extras"
 import { Patient } from "@/types/IPatient"
 import { useEffect, useState } from "react"
-import { FlatList, StyleSheet } from "react-native"
+import { FlatList, StyleSheet, Text, View } from "react-native"
 import { PatientListItem } from "./item"
 
 interface PatientListProps {
@@ -12,6 +12,8 @@ interface PatientListProps {
 
 export const PatientList: React.FC<PatientListProps> = ({ handleClick }) => {
   const [patientsList, setPatients] = useState<Patient[]>([]);
+  const [textPatient, setTextPatient] = useState<string>("")
+
   useEffect(() => {
     const fetchPatients = async () => {
       // Simulando uma chamada de API 
@@ -20,7 +22,8 @@ export const PatientList: React.FC<PatientListProps> = ({ handleClick }) => {
       // const data = await response.json();
       // setPatients(data);
       // Para este exemplo, estamos apenas usando a lista estática
-      setPatients(PatientsData);
+      const data = PatientsData.sort((a, b) => a.name.localeCompare(b.name))
+      setPatients(data);
     };
     fetchPatients();
   }, []);
@@ -29,29 +32,52 @@ export const PatientList: React.FC<PatientListProps> = ({ handleClick }) => {
     handleClick(item);
   };
 
+  const handleChange = (data: string) => {
+    setTextPatient(data)
+  }
+
   return (
-    <FlatList
-      data={patientsList}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => <PatientListItem patient={item} handlePress={handlePress} />}
-      style={styles.container}
-      getItemLayout={(_, index) => ({
-        length: LIST_ITEM_HEIGHT,
-        offset: LIST_ITEM_HEIGHT * index,
-        index,
-      })}
-      showsVerticalScrollIndicator={false}
-    />
+    <View style={styles.listView}>
+      <Text style={styles.listTitle}>Lista de Pacientes</Text>
+      {/* <TextInput value={textPatient} onChangeText={handleChange} /> */}
+      <FlatList
+        data={patientsList}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <PatientListItem patient={item} handlePress={handlePress} />}
+        style={styles.container}
+        getItemLayout={(_, index) => ({
+          length: LIST_ITEM_HEIGHT,
+          offset: LIST_ITEM_HEIGHT * index,
+          index,
+        })}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.white,
     borderRadius: 8,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 5,
-  }
+  },
+  listView: {
+    height: LIST_ITEM_HEIGHT * 8 + 20,
+    borderWidth: 2,
+    borderColor: Colors.darkgreen,
+    borderRadius: 8
+  },
+  listTitle: {
+    padding: 10,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    textDecorationStyle: 'double',
+    textDecorationColor: Colors.darkgreen,
+    textDecorationLine: 'underline'
+  },
 });
--- components/PatientList/item.tsx
+++ components/PatientList/item.tsx
@@ -1,4 +1,5 @@
 import { ConsultsData, HealthPlansData } from "@/constants/API";
+import { Colors } from "@/constants/Colors";
 import { LIST_ITEM_HEIGHT } from "@/constants/Extras";
 import { convertDate } from "@/lib/date";
 import { Consult } from "@/types/IConsult";
@@ -6,7 +7,6 @@ import { HealthPlan } from "@/types/IHealthPlan";
 import { Patient } from "@/types/IPatient";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 interface PatientItemProps {
     patient: Patient;
@@ -28,6 +28,7 @@ export const PatientListItem: React.FC<PatientItemProps> = ({ patient, handlePre
             setNumberOfConsults(FilterConsults.length)
             setLastConsult(FilterConsults.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).pop())
         }
+        fetchConsults()
     }, []);
 
 
@@ -36,34 +37,36 @@ export const PatientListItem: React.FC<PatientItemProps> = ({ patient, handlePre
             height: LIST_ITEM_HEIGHT * 2,
             borderBottomWidth: 2,
             borderColor: Colors.lightgreen,
+            backgroundColor: patient.finished ? Colors.darkgreen : Colors.lightgreen,
             borderRadius: 8,
-            paddingVertical: 10
+            paddingVertical: 10,
         },
         itemTitle: {
             fontSize: 18,
             fontWeight: 'bold',
             marginTop: 5,
             textAlign: 'center',
-            color: Colors.darkgreen
+            color: patient.finished ? Colors.lightgreen : Colors.darkgreen
         },
         itemSubTitle: {
             fontSize: 16,
             fontWeight: 'semibold',
             padding: 5,
-            color: Colors.darkgreen
+            color: patient.finished ? Colors.lightgreen : Colors.darkgreen
         },
         itemText: {
             fontStyle: 'italic',
             paddingHorizontal: 5,
-            color: Colors.darkgreen
+            color: patient.finished ? Colors.lightgreen : Colors.darkgreen
         }
     })
 
     return (
         <TouchableOpacity style={stylesPatient.item} onPress={() => handlePress(patient)}>
-            <Text style={stylesPatient.itemTitle}>{patient.name}</Text>
-            <Text style={stylesPatient.itemSubTitle}>{patient.age} anos - Plano: {healthPlan?.name}</Text>
-            <Text style={stylesPatient.itemText}>Consultas: {numberOfConsults} - Ult. Consulta: {convertDate(lastConsult?.date)}</Text>
+            <Text style={stylesPatient.itemTitle}>{patient.name} - {patient.finished && "Alta"}</Text>
+            <Text style={stylesPatient.itemSubTitle}>Plano: {healthPlan?.name}</Text>
+            <Text style={stylesPatient.itemText}>Consultas: {numberOfConsults}</Text>
+            <Text style={stylesPatient.itemText}>Últ. Consulta: {convertDate(lastConsult?.date)}</Text>
         </TouchableOpacity>
     )
 };

67ce046135d0f68a7a99456fac4c0695860088e3
ebrauta <ebrauta@gmail.com>
2025-05-16 22:46:28 -0300

    refactor: simplificando o layout iniciail do Expo e excluindo arquivos não utilizados

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,33 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const Icon: React.FC<{name: any, color: string, size:number}> = ({name, color, size}) => {
+  return <Ionicons name={name} size={size} color={color} />;
+}
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        headerShown: false
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: () => <Icon size={28} name="home-outline" color="#3F888F" />,
+          tabBarLabelStyle: { fontSize: 12, color: '#3F888F' },
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="patients"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Pacientes',
+          tabBarIcon: () => <Icon size={28} name="people-circle-outline" color="#3F888F" />,
+          tabBarLabelStyle: { fontSize: 12, color: '#3F888F' },
         }}
       />
     </Tabs>
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,7 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+  white: '#f2f2f2',
+  black: '#020202',
+  gray: '#808080',
+  lightgreen: '#95F0C4',
+  darkgreen: '#3F888F',
 };
